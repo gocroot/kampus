456e6ce61f4ba7a0f7c549b6dc21f3cd2961acb7
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2023-06-26 22:55:21 +0800

    golang

---
+++ model/1214061.go
@@ -0,0 +1,25 @@
+package nizar
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type DataTagihan struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Nama_Mahasiswa string             `bson:"nama_mahasiswa" json:"nama_mahasiswa"`
+	Program_studi  string             `bson:"program_studi" json:"program_studi"`
+	Jumlah  string             `bson:"jumlah" json:"jumlah"`
+	Status  string             `bson:"status" json:"status"`
+}
+type DataSudah struct {
+	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Nama_Mahasiswa string             `bson:"nama_mahasiswa" json:"nama_mahasiswa"`
+	Status   string          `bson:"status" json:"status"`
+}
+
+type DataBelum struct {
+	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Nama_Mahasiswa string             `bson:"nama_mahasiswa" json:"nama_mahasiswa"`
+	Status   string          `bson:"status" json:"status"`
+
+}
